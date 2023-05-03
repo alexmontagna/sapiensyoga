8986349e9b07ab6ac3df1e490dac8d61c0686ba1
Alexandre <alexandremontagna@gmail.com>
2023-05-03 20:20:31 +0200

    REMOVE sensitive information

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import YogaComponent from './YogaComponent'
+import ColorToggle from './ColorToggle'
+
+import { createNotionClient, fetchNotionData } from './notionClient';
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
     <YogaComponent />
+    <ColorToggle />
   </React.StrictMode>,
 )
