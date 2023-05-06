a9fc5d77f7bdf07bc9add11016d7c9eb01b86a93
Alexandre <alexandremontagna@gmail.com>
2023-05-06 22:29:58 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from './assets/vite.svg'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('banana')
 
   return (
     <div className="App">
