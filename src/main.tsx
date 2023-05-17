851d19638625c20f02e586c1be4952c9a661c794
Alexandre <alexandremontagna@gmail.com>
2023-05-17 15:49:01 +0200

    Hide color toggle

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
     <YogaComponent />
-    <ColorToggle />
+    {/* <ColorToggle /> */}
   </React.StrictMode>,
 )
